528e277756ae8ed61188d2711da30103bc193fd5
charmi-v <charmi.vyas@smartsensesolutions.com>
2024-10-22 18:13:16 +0530

    test: adding code coverage

--- vite.config.ts
+++ vite.config.ts
@@ -21,10 +21,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import svgr from 'vite-plugin-svgr'
+import istanbul from 'vite-plugin-istanbul'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), tsconfigPaths()],
+  plugins: [
+    react(),
+    svgr(),
+    tsconfigPaths(),
+    istanbul({
+      include: 'src/*',
+      exclude: ['node_modules', 'test/'],
+      extension: ['.js', '.ts', '.vue'],
+      requireEnv: false,
+    }),
+  ],
   optimizeDeps: { exclude: ['fsevents'] },
   build: {
     outDir: 'build',
